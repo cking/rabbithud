eea178b4e0b609ded3a0f0cab5f6289591f8233d
Kura <me@kurabloodlust.eu>
2022-10-14 10:21:50 +0200

    overlay url structure

---
+++ packages/renderer/src/lib/url.js
@@ -0,0 +1,22 @@
+class OverlayURL {
+  // TODO: provide a way to generate url from JSON instead directly providing this sht
+  constructor (path, modern = false, options = '') {
+    this.path = path
+    this.modern = !!modern
+    this.options = '' + options
+  }
+
+  compute () {
+    // TODO: proxy instead of original
+    const url = new URL(path)
+
+    // TODO: wss wrapper
+    // TODO: get ip and port from global config object
+    url.search = (this.modern
+      ? '?OVERLAY_WS=ws://' + ipAddress + ':' + port + '/ws'
+      : '?HOST_PORT=ws://' + ipAddress + ':' + port) +
+      this.options
+
+    return url
+  }
+}
