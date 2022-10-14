7a688a0b51eb18b8b1096f42bcb31c598637fc47
Kura <me@kurabloodlust.eu>
2022-10-14 15:27:43 +0200

    general settings mock up

---
+++ packages/renderer/src/views/GeneralConfig.svelte
@@ -0,0 +1,42 @@
+<fieldset>
+    <legend>IINACT</legend>
+
+    <div>
+        <label for="ip">IP</label>
+        <input id="ip">
+    </div>
+    <div>
+        <label for="port">Port</label>
+        <input id="port">
+    </div>
+    <div>
+        <label>
+            <input type="checkbox"> Enable Websocket Secure Proxy (EXPERIMENTAL!)
+        </label>
+    </div>
+</fieldset>
+
+<fieldset>
+    <legend>Cactbot</legend>
+
+    <div>
+        <label>
+            <input type="checkbox"> Enable
+        </label>
+    </div>
+</fieldset>
+
+<fieldset>
+    <legend>Startup</legend>
+
+    <div>
+        <label>
+            <input type="checkbox"> Display Overlays on Start
+        </label>
+    </div>
+    <div>
+        <label for="hotkey">Hotkey to toggle all Overlays</label>
+        <button>❌ Clear</button>
+        <input id="hotkey">
+    </div>
+</fieldset>
