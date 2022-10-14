42960cbfc87b55569d08415bd97281827f1983c7
Kura <me@kurabloodlust.eu>
2022-10-14 14:43:42 +0200

    cactbot config

--- packages/preload/src/index.js
+++ packages/preload/src/index.js
@@ -3,5 +3,5 @@ import { contextBridge, ipcRenderer } from 'electron'
 contextBridge.exposeInMainWorld('rabbit', {
   devtools: () => ipcRenderer.send('toggle-devtools'),
   updateConfig: (config) => ipcRenderer.send('update-config', config),
-  readConfig: () => ipcRenderer.invoke('toggle-devtools')
+  readConfig: () => ipcRenderer.invoke('get-config')
 })
--- packages/renderer/src/views/Cactbot.svelte
+++ packages/renderer/src/views/Cactbot.svelte
@@ -4,4 +4,12 @@
     const cactbotUrl = new OverlayURL("https://quisquous.github.io/cactbot/ui/config/config.html", true).compute()
 </script>
 
+<style>
+    iframe {
+        border: none;
+        width: 100%;
+        height: 100%;
+    }
+</style>
+
 <iframe title="Cactbot Settings" src={cactbotUrl}></iframe>
