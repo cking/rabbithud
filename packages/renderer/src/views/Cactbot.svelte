140975dd1fe1b4f5a223d952aa7e916d89db5b0b
Kura <me@kurabloodlust.eu>
2022-10-16 10:44:00 +0200

    cactbot from preset

--- packages/renderer/src/views/Cactbot.svelte
+++ packages/renderer/src/views/Cactbot.svelte
@@ -1,7 +1,7 @@
 <script>
     import { OverlayURL } from "../lib/url"
 
-    const cactbotUrl = new OverlayURL("https://quisquous.github.io/cactbot/ui/config/config.html", true).compute()
+    const cactbotUrl = OverlayURL.fromPreset("cactbot_config").compute()
 </script>
 
 <style>
